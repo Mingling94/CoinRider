45e95af3c4866934fd7186d28f78d7d96aff9552
ror5226 <ror5226@psu.edu>
2015-03-29 03:51:39 -0400

    Made a wallet and menu junk

--- Assets/WalletCreate.cs
+++ Assets/WalletCreate.cs
@@ -1,6 +1,16 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+public class JResult {
+	public string address { get; set; }
+}
+
 
 public class WalletCreate : MonoBehaviour {
 
@@ -25,8 +35,33 @@ public class WalletCreate : MonoBehaviour {
 	//	Application.LoadLevel (word);
 		strEmail = email.text;
 		strPassword = password.text;
-		Debug.Log(strEmail);
-		Debug.Log (strPassword);
+		string url = "https://blockchain.info/api/v2/create_wallet?email=" + 
+			strEmail + "&password=" + strPassword + 
+			"&api_code=82d78766-eae4-4aa2-914e-95ad9931d242";
+
+		string[] stringSeparators = new string[] {"address\":\""};
+		string[] stringSeparators2 = new string[] {"\",\"link"};
+		string[] m;
+
+		WebRequest request = WebRequest.Create (url);
+		WebResponse response = request.GetResponse ();
+
+		Debug.Log (((HttpWebResponse)response).StatusDescription);
+
+		Stream dataStream = response.GetResponseStream ();
+		StreamReader reader = new StreamReader (dataStream);
+
+		string responseFromServer = reader.ReadToEnd ();
+
+
+		m =  responseFromServer.Split (stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+		m = m[1].Split (stringSeparators2, StringSplitOptions.RemoveEmptyEntries);
+
+		Debug.Log (m[0]);
+
+
+		reader.Close ();
+		response.Close ();
 	}
 
 }
