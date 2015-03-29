03fb2a0f5536b45ea44640543c878719ec5c84c0
ror5226 <ror5226@psu.edu>
2015-03-29 11:12:50 -0400

    FInished Menu

---
+++ Assets/loginAddress.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class loginAddress : MonoBehaviour {
+
+	public InputField login;
+
+	private string address;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 
+	}
+
+	void SendMessage(string word){
+		Application.LoadLevel (word);
+		address = login.text;
+	}
+}
